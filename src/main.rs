80e8b92dad64b9a11ee77783e6af05e26fb63abf
PraxTube <lukinator28@gmail.com>
2023-07-29 13:45:19 +0200

    restructure!: Split logic and make more modular

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use reqwest;
+use reqwest::{self, Response};
 use serde;
 
 #[derive(Debug, serde::Deserialize, serde::Serialize)]
@@ -13,6 +13,23 @@ struct FeeRecommendation {
     economy_fee: u32,
     #[serde(rename = "minimumFee")]
     minimum_fee: u32,
+}
+
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+struct BlockData {
+    id: String,
+    height: u32,
+    version: u32,
+    timestamp: u64,
+    tx_count: u32,
+    size: u32,
+    weight: u32,
+    merkle_root: String,
+    previousblockhash: String,
+    mediantime: u64,
+    nonce: u32,
+    bits: u32,
+    difficulty: u64,
 }
 
 fn stylize_string(input_str: &str) -> String {
@@ -65,6 +82,22 @@ fn stylize_string(input_str: &str) -> String {
     output_str
 }
 
+fn positive_response(response: &Response) -> bool {
+    match response.status() {
+        reqwest::StatusCode::OK => return true,
+        reqwest::StatusCode::UNAUTHORIZED => {
+            println!("Unauthorized...");
+        }
+        reqwest::StatusCode::TOO_MANY_REQUESTS => {
+            println!("Exceeded rate limits, too many requests.");
+        }
+        _ => {
+            panic!("Not known error happend.");
+        }
+    }
+    false
+}
+
 fn print_fee(fees: FeeRecommendation) {
     let fee_str = stylize_string(&format!(
         "Fees in sats/vB\n\
@@ -74,31 +107,63 @@ fn print_fee(fees: FeeRecommendation) {
         fees.fastest_fee, fees.hour_fee, fees.minimum_fee
     ));
     println!("{}", fee_str);
+    let expected_fee = stylize_string(&format!(
+        "\nExpected fees in sats:\n\
+            [color=#BF0000]High end:[/color] {}, [color=#009F4F]Low end:[/color] {}",
+        fees.fastest_fee * 600,
+        fees.minimum_fee * 200
+    ));
+    println!("{}", expected_fee);
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn print_block(block: BlockData) {
+    println!("{}", block.height);
+}
+
+async fn do_fee() -> Result<(), Box<dyn std::error::Error>> {
     let endpoint_url = "https://mempool.space/api/v1/fees/recommended";
     println!("Fetching data...");
     let response = reqwest::get(endpoint_url).await?;
 
-    match response.status() {
-        reqwest::StatusCode::OK => {
-            match response.json::<FeeRecommendation>().await {
-                Ok(parsed) => print_fee(parsed),
-                Err(err) => println!(
-                    "Hm, the response didn't match the shape we expected.\n{}",
-                    err
-                ),
-            };
-        }
-        reqwest::StatusCode::UNAUTHORIZED => {
-            println!("Unauthorized...");
-        }
-        _ => {
-            panic!("Not known error happend.");
-        }
+    if positive_response(&response) {
+        match response.json::<FeeRecommendation>().await {
+            Ok(parsed) => print_fee(parsed),
+            Err(err) => println!("MISMATCH, didn't match expected shape.\n{}", err),
+        };
     }
+    Ok(())
+}
 
+async fn get_hash() -> Result<String, Box<dyn std::error::Error>> {
+    let endpoint_hash = "https://mempool.space/api/blocks/tip/hash";
+    println!("Fetching data...");
+    let response = reqwest::get(endpoint_hash).await?;
+
+    if positive_response(&response) {
+        let hash = response.text().await?;
+        return Ok(hash);
+    }
+    Ok(String::new())
+}
+
+async fn do_block() -> Result<(), Box<dyn std::error::Error>> {
+    let hash = get_hash().await?;
+    let endpoint_url = "https://mempool.space/api/block/".to_string() + &hash;
+    println!("Fetching data...");
+    let response = reqwest::get(endpoint_url).await?;
+
+    if positive_response(&response) {
+        match response.json::<BlockData>().await {
+            Ok(parsed) => print_block(parsed),
+            Err(err) => println!("MISTMATCH, didn't match the shape expected.\n{}", err),
+        };
+    }
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    do_fee().await?;
+    do_block().await?;
     Ok(())
 }
