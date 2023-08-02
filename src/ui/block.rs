5b09603c3502d6fb91dd6525f94dc572915e95d0
PraxTube <lukinator28@gmail.com>
2023-08-02 09:01:51 +0200

    chore: Trim text if block width too small

--- src/ui/block.rs
+++ src/ui/block.rs
@@ -5,9 +5,11 @@ const BLOCK_FILL_COLOR_INDEX: u8 = 69;
 
 fn write_row(message: &mut Vec<Vec<char>>, row: i32, content: &str) {
     let width = message[0].len();
-    if width - 2 < content.chars().count() {
-        panic!("Width is too small.");
-    }
+    let content = if width - 2 < content.chars().count() {
+        &content[0..width - 1]
+    } else {
+        content
+    };
 
     let row: usize = match row < 0 {
         true => message.len() - row.wrapping_abs() as usize,
@@ -26,12 +28,14 @@ pub fn render(width: usize, height: usize, block: &BlockData) -> Vec<String> {
     if width < 2 || height < 2 {
         panic!("Width and height must be at least 2.");
     }
-    let bheight = utils::format_number_kilo(block.height, "_").into_bytes();
-    if width < bheight.len() + 2 {
-        panic!("Width must be bigger then the number of digits in block height");
-    }
+    let title = utils::format_number_kilo(block.height, "_").into_bytes();
+    let title = if width < title.len() + 2 {
+        title[0..width - 1].to_vec()
+    } else {
+        title
+    };
 
-    let title_start_index = width / 2 - utils::div_up(bheight.len(), 2);
+    let title_start_index = width / 2 - utils::div_up(title.len(), 2);
     let mut result: Vec<String> = Vec::new();
     // TODO: Fix this size, which is the reason
     // we get bad positioning (should be smaller then width and height)
@@ -40,15 +44,24 @@ pub fn render(width: usize, height: usize, block: &BlockData) -> Vec<String> {
 
     write_row(
         &mut message,
-        3,
-        &format!("{}B", utils::format_number_unit(block.size)),
+        1,
+        &format!("\u{2248} {:.0} sat/vB", block.extras.median_fee),
     );
-    write_row(&mut message, 4, &format!("{} txs", block.tx_count));
     write_row(
         &mut message,
-        5,
-        &format!("{}WU", utils::format_number_unit(block.weight)),
+        2,
+        &format!(
+            "{:.0} - {:.0} sat/vB",
+            block.extras.fee_range[0],
+            block.extras.fee_range[block.extras.fee_range.len() - 1]
+        ),
     );
+    write_row(
+        &mut message,
+        4,
+        &format!("{}B", utils::format_number_unit(block.size)),
+    );
+    write_row(&mut message, 5, &format!("{} txs", block.tx_count));
     write_row(
         &mut message,
         -2,
@@ -59,8 +72,8 @@ pub fn render(width: usize, height: usize, block: &BlockData) -> Vec<String> {
     result.push(String::new());
     result[0].push('╭');
     for i in 1..width - 1 {
-        if i >= title_start_index && i < title_start_index + bheight.len() {
-            result[0].push(bheight[i - title_start_index] as char);
+        if i >= title_start_index && i < title_start_index + title.len() {
+            result[0].push(title[i - title_start_index] as char);
         } else {
             result[0].push('─');
         }
